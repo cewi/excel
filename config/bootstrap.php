9fb5aa628ff062027efad63fe34bfce88b722ed7
Cornelius Wichmann <c.wichmann@gmx.de>
2016-01-03 16:13:57 +0100

    Changes for Cake 3.1 and improved Date handling
    setting viewClassMap ist deprecated
    set default format for Dates when exported to Excel

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,7 +1,8 @@
 <?php
 
-use Cake\Event\EventManager;
+use Cake\Core\Configure;
 use Cake\Datasource\ConnectionManager;
+use Cake\Event\EventManager;
 
 /**
  * use sqlite for temporary Data
@@ -25,8 +26,13 @@ EventManager::instance()
                 function (Cake\Event\Event $event) {
             $controller = $event->subject();
             if ($controller->components()->has('RequestHandler')) {
-                $controller->RequestHandler->viewClassMap('xlsx', 'Cewi/Excel.Excel');
+                $controller->RequestHandler->config('viewClassMap.xlsx', 'Cewi/Excel.Excel');
             }
         }, 'Controller.initialize'
 );
 
+/**
+ * because Cake3 returns instances of Cake\i18n\Time for datetime-fields, it is necessary to tell the Helper
+ * in which format they should be exported to excel
+ */
+Configure::write('excel.dateformat', 'dd.MM.YYYY');
